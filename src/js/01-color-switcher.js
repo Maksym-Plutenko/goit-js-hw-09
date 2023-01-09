3a0e60ba52e8e455e8c712a64eabfe2109edf2d4
Maksym-Plutenko <plutenkom@gmail.com>
2023-01-09 17:09:06 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId;
+
+stoptBtn.disabled = true;
+
+startBtn.addEventListener('click', start);
+stoptBtn.addEventListener('click', stop);
+
+function start(event) {
+    changeColor();
+    timerId = setInterval(changeColor, 1000);
+    startBtn.disabled = true;
+    stoptBtn.disabled = false;
+}
+
+function stop(event) {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stoptBtn.disabled = true;
+}
+
+function changeColor() {
+    body.style.backgroundColor = getRandomHexColor();
+}
