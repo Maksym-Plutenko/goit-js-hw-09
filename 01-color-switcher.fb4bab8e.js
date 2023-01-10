e4fb9cae0bab14af7aa94ffe4cd1240fbc6bda26
Maksym-Plutenko <110676182+Maksym-Plutenko@users.noreply.github.com>
2023-01-10 08:21:32 +0000

    Deploying to gh-pages from @ Maksym-Plutenko/goit-js-hw-09@3a0e60ba52e8e455e8c712a64eabfe2109edf2d4 🚀

---
+++ 01-color-switcher.fb4bab8e.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),d=document.querySelector("body");function o(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}n.disabled=!0,e.addEventListener("click",(function(d){o(),t=setInterval(o,1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(d){clearInterval(t),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.fb4bab8e.js.map
